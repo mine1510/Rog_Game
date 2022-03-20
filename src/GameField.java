5c28e8ed706b3f47cc65a1fbdf9c7cb1cacc30fc
MISQZY <npershutov@gmail.com>
2022-03-20 17:39:48 +0300

    Added GameField class

---
+++ src/GameField.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class GameField extends JPanel {
+    public GameField(){
+
+    }
+}
--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,2 +1,16 @@
-public class MainWindow {
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        super("Roguelike");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(320,345);
+        setLocation(400,400);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+    }
 }
